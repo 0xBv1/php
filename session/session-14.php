80eceb65ea9208883fa6cb1d54a808c06b02835e
BV1 <47463933+bedoadly1121@users.noreply.github.com>
2024-08-09 21:29:57 +0300

    Create session-14.php

---
+++ session/session-14.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+database  
+sql : structured query lang
+
+mysql: database sys => mssql, oracle
+
+mysql or mysqli => extention
+
+PDO => php data objects -> oop
+dbms -> relation db ->mysql, oracle, access, sql server, db, progress
+     -> norelation db  no sql-> okiject => mongoDB
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
